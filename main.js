09d91ce61c1eca676b47c41ebd8b51de58f22555
Michel Barnich <32980248+DerM1chi@users.noreply.github.com>
2020-05-29 19:54:28 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 function adjustZoom() {
     //replace 1920 by the static width your HTML and CSS is set to. If you set your body width to 800px, set replace 1920 by 800.
+    //in the HTML body tag, set these attributes: onload="adjustZoom()" and onresize="adjustZoom()".
+    //see an example usecase on https://michelbarnich.com
     document.body.style.zoom = (document.documentElement.clientWidth / 1920  * 100) + "%";
 }
